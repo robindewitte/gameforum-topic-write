a7243ce63c190f0e3be6861058266261ed1e786a
Robin de Witte <r.dewitte@student.fontys.nl>
2021-10-14 15:49:19 +0200

    cqrs en rabbitmq werkend

--- writeservice/Messaging/MessageConsumer.cs
+++ writeservice/Messaging/MessageConsumer.cs
@@ -1,41 +1,68 @@
-﻿using RabbitMQ.Client;
+﻿using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using writeservice.Controllers;
 
 namespace writeservice.Messaging
 {
-    public static class MessageConsumer
+    public class MessageConsumer: BackgroundService
     {
-        public static void Consume()
+        private ConnectionFactory _factory;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public MessageConsumer()
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
+            _factory = new ConnectionFactory() { HostName = "localhost" };
+
+            _connection = _factory.CreateConnection();
+
+            _channel = _connection.CreateModel();
+
+            _channel.QueueDeclare(
+                queue: "write",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            WriteController writeController = new WriteController();
+
+            if (stoppingToken.IsCancellationRequested)
             {
-                channel.QueueDeclare(queue: "write",
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
-
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    var body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] Received {0}", message);
-                };
-                channel.BasicConsume(queue: "hello",
-                                     autoAck: true,
-                                     consumer: consumer);
-
-                Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
+                _channel.Dispose();
+                _connection.Dispose();
+                return Task.CompletedTask;
             }
+
+            var consumer = new EventingBasicConsumer(_channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                writeController.DoWriteStuffMock();
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+             _channel.BasicConsume(queue: "write", autoAck: false, consumer: consumer);
+            return Task.CompletedTask;
         }
     }
 }
--- writeservice/Startup.cs
+++ writeservice/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using writeservice.Messaging;
 
 namespace writeservice
 {
@@ -32,6 +33,7 @@ namespace writeservice
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "writeservice", Version = "v1" });
             });
+            services.AddHostedService<MessageConsumer>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
